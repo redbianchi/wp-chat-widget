1d05eb0da23c637dec67a00faa5d42696b70b808
Christopher Chaput <christopher.chaput@gmail.com>
2023-07-12 10:10:06 -0400

    url pusher

--- index.js
+++ index.js
@@ -1,58 +1,21 @@
 const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-require('dotenv').config();
-const cheerio = require('cheerio');
-
 const app = express();
 
 
-// Enable CORS
-app.use(cors());
-
-// Middleware to parse JSON request body
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post('/ask', async (req, res) => {
-    const { question, url } = req.body; // Access the URL from the request payload
-  
-    try {
-      // Fetch the webpage content using the provided URL
-      const response = await axios.get(url);
-  
-      // Extract the desired text from the webpage using cheerio or any other parsing library
-      const $ = cheerio.load(response.data);
-      const text = $('body').text();
-  
-      // Include the text in the prompt for question-answering
-      const prompt = `${text}\n\nQ: ${question}\nA:`;
-  
-      // Call the OpenAI API to get the answer
-      const openAIResponse = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-        prompt,
-        max_tokens: 200,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-      });
-  
-      const answer = openAIResponse.data.choices[0].text.trim();
-  
-      // Send the answer back to the client
-      res.json({ answer });
-    } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-  
-  // ...
-  
-  app.all('/ask', (req, res) => {
-    res.status(405).json({ error: 'Method Not Allowed' });
-  });
+app.post('/submit-url', (req, res) => {
+  const { url } = req.body;
+  // Process the URL or perform any desired actions with it
+  console.log('Received URL:', url);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
   
 
 // Start the server
