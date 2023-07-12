ea647c062ba79f00645ba894c5d05c35f70cbd38
Christopher Chaput <christopher.chaput@gmail.com>
2023-07-12 11:22:43 -0400

    adding openai

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const openai = require('openai');
+const app = express();
+
 app.use(cors());
+
+openai.apiKey = process.env.OPENAI_API_KEY;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +19,23 @@ app.post('/submit-url', (req, res) => {
   res.send('URL and content received');
 });
 
-
+app.post('/ask-question', async (req, res) => {
+    const { content, question } = req.body;
+    const prompt = `I read this article: ${content}. ${question}`;
+  
+    try {
+      const gptResponse = await openai.Completion.create({
+        engine: 'text-davinci-002',
+        prompt: prompt,
+        max_tokens: 60
+      });
+  
+      res.send(gptResponse.choices[0].text.strip());
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while processing your request.');
+    }
+  });
 
   
 
